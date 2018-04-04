1e34e1478bd0ba153c8846fc7ea8f167f85c4e62
Aleksi Alajoki <c6alal00@students.oamk.fi>
2018-04-04 18:38:40 +0300

    Pelaajan kontrollit, ampuminen, kamera

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'gameDiv', { preload: preload, create: create, update: update, render: render });
+var mouseTouchDown = false;
 
-function preload() 
+function preload()
 {
     game.time.advancedTiming = true;
     game.load.image('wall', 'sprites/wall.png');
     game.load.image('floor', 'sprites/floor.png');
+    game.load.image('player', 'sprites/player.png');
+    game.load.image('projectile', 'sprites/projectile.png');
 }
 
 
-function create() 
+function create()
 {
     cursors = game.input.keyboard.createCursorKeys();
-    space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);  
+    space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    W_key = game.input.keyboard.addKey(Phaser.Keyboard.W)
+    A_key = game.input.keyboard.addKey(Phaser.Keyboard.A)
+    S_key = game.input.keyboard.addKey(Phaser.Keyboard.S)
+    D_key = game.input.keyboard.addKey(Phaser.Keyboard.D)
+
+    // Physics
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = 0;
+
 
     game.stage.backgroundColor = '#000';
 
@@ -20,7 +33,7 @@ function create()
 
     // At minimum define the sprite keys you would like to use for the walls and floor of the dungeon
     //Tilesize needs to be changed in the dungeon-generator.min.js file, otherwise it'll crash
-    var dungeonParams = 
+    var dungeonParams =
     {
         'wall' : 'wall',
         'floor' : 'floor',
@@ -33,40 +46,112 @@ function create()
 
     // Actually the Dungeon
     dungeonCreator.createMap();
+
+
+
+
+    // Create player
+
+    //playergroup = game.add.group();
+    player = game.add.sprite(100, 100, 'player');
+    game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
+    player.anchor.set(0.5);
+    player.body.allowRotation = false;
+
+    // Create projectiles
+    projectiles = game.add.group();
+    projectiles.enableBody = true;
+    projectiles.physicsBodyType = Phaser.Physics.ARCADE;
+    // Create several projectiles for use
+    projectiles.createMultiple(50, 'projectile');
+    // Execute the reset function for each projectile that goes out of the world bounds
+    projectiles.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetProjectile);
+    projectiles.callAll('anchor.setTo','anchor', 0.5, 1.0);
+    projectiles.setAll('checkWorldBounds', true);
+
+
 }
 
-function update() 
+
+
+function update()
 {
     // If the space bar is being pressed, generate a new dungeon
-    if ( space.isDown ) 
+    if ( space.isDown )
     {
         dungeonCreator.destroyMap();
         dungeonCreator.createMap();
     }
 
-    // Scroll the camera up and down
-    if (cursors.up.isDown)
-    {
-        game.camera.y -= 4;
-    }
-    else if (cursors.down.isDown)
-    {
-        game.camera.y += 4;
-    }
 
-    // Scroll the camera left and right
-    if (cursors.left.isDown)
-    {
-        game.camera.x -= 4;
+
+
+
+    // Camera controls + camera follows the player
+    player.body.velocity.y = 0;
+    player.body.velocity.x = 0;
+
+    if(W_key.isDown) {
+      player.body.velocity.y -= 150;
     }
-    else if (cursors.right.isDown)
-    {
-        game.camera.x += 4;
+    else if(S_key.isDown) {
+      player.body.velocity.y += 150;
+    }
+    if(A_key.isDown) {
+      player.body.velocity.x -= 150;
+    }
+    else if(D_key.isDown) {
+      player.body.velocity.x += 150;
+    }
+    game.camera.follow(player);
+
+
+    // Player rotation + shooting
+    player.rotation = game.physics.arcade.angleToPointer(player);
+
+    if (game.input.activePointer.isDown) {
+      if (!mouseTouchDown) {
+        touchDown();
+      }
+    } else {
+      if (mouseTouchDown) {
+        touchUp();
+      }
     }
 
 }
 
-function render() 
+function render()
 {
 	game.debug.text('FPS: ' + game.time.fps, 2, 14, "#00ff00");
+}
+
+
+
+
+
+
+
+function resetProjectile(projectile) {
+  projectile.kill();
+}
+
+function fireProjectile() {
+  var projectile = projectiles.getFirstExists(false);
+  if (projectile) {
+    projectile.reset(player.x, player.y); // Projectile origin point
+    game.physics.arcade.moveToPointer(projectile, 600); // Projectile speed
+  }
+}
+
+function touchDown() {
+	// Set touchDown to true, so we only trigger this once
+	mouseTouchDown = true;
+	fireProjectile();
+}
+
+function touchUp() {
+	// Set touchDown to false, so we can trigger touchDown on the next click
+	mouseTouchDown = false;
 }
