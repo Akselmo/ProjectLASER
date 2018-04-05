5a3caf3367cdd77bef32cd65f92f37f636365bd5
Akseli Lahtinen <n6laak00@students.oamk.fi>
2018-04-05 16:02:53 +0300

    New dungeon generator, placing player in random place of the map
    Had to add a new dungeon generator completely so it'll work better.
    
    Made player character spawn in random spot inside the dungeon and collide with dungeon walls
    
    Added custom sprites to see how the game would look

--- js/main.js
+++ js/main.js
@@ -1,124 +1,114 @@
-var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'gameDiv', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(1280, 768, Phaser.AUTO, 'gameDiv', { preload: preload, create: create, update: update, render: render });
 var mouseTouchDown = false;
+var player;
 
 function preload()
 {
-    game.time.advancedTiming = true;
-    game.load.image('wall', 'sprites/wall.png');
-    game.load.image('floor', 'sprites/floor.png');
-    game.load.image('player', 'sprites/player.png');
-    game.load.image('projectile', 'sprites/projectile.png');
+  game.time.advancedTiming = true;
+  //Floor tile needs to be twice as big as wall! This can be done by tiling (2x2) the floor texture!
+  //Wall 128x128, floor 256x256
+  game.load.image('wall', 'sprites/Swall1.png');
+  game.load.image('floor', 'sprites/Sfloor4.png');
+  game.load.image('player', 'sprites/player.png');
+  game.load.image('projectile', 'sprites/projectile.png');
 }
 
 
 function create()
 {
-    cursors = game.input.keyboard.createCursorKeys();
-    space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  cursors = game.input.keyboard.createCursorKeys();
+  space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-    W_key = game.input.keyboard.addKey(Phaser.Keyboard.W)
-    A_key = game.input.keyboard.addKey(Phaser.Keyboard.A)
-    S_key = game.input.keyboard.addKey(Phaser.Keyboard.S)
-    D_key = game.input.keyboard.addKey(Phaser.Keyboard.D)
+  W_key = game.input.keyboard.addKey(Phaser.Keyboard.W)
+  A_key = game.input.keyboard.addKey(Phaser.Keyboard.A)
+  S_key = game.input.keyboard.addKey(Phaser.Keyboard.S)
+  D_key = game.input.keyboard.addKey(Phaser.Keyboard.D)
 
-    // Physics
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.physics.arcade.gravity.y = 0;
-
-
-    game.stage.backgroundColor = '#000';
-
-    // Initialise the Dungeon Creation Plugin
-    dungeonCreator = game.plugins.add(Phaser.Plugin.DungeonCreator);
-
-    // At minimum define the sprite keys you would like to use for the walls and floor of the dungeon
-    //Tilesize needs to be changed in the dungeon-generator.min.js file, otherwise it'll crash
-    var dungeonParams =
-    {
-        'wall' : 'wall',
-        'floor' : 'floor',
-        'map_size_x' : '2200',
-        'map_size_y' : '2200'
-    };
-
-    // Set some parameters for your required Dungeon
-    dungeonCreator.setupDungeon( dungeonParams );
-
-    // Actually the Dungeon
-    dungeonCreator.createMap();
+  // Physics
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.arcade.gravity.y = 0;
 
 
+  game.stage.backgroundColor = '#000';
+
+  
+  //playergroup = game.add.group();
+  player = game.add.sprite(100, 100, 'player');
+  game.physics.arcade.enable(player);
+  player.body.collideWorldBounds = true;
+  player.anchor.set(0.5);
+  player.body.allowRotation = false;
+  
+
+  // Create projectiles
+  projectiles = game.add.group();
+  projectiles.enableBody = true;
+  projectiles.physicsBodyType = Phaser.Physics.ARCADE;
+  // Create several projectiles for use
+  projectiles.createMultiple(50, 'projectile');
+  // Execute the reset function for each projectile that goes out of the world bounds
+  projectiles.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetProjectile);
+  projectiles.callAll('anchor.setTo','anchor', 0.5, 1.0);
+  projectiles.setAll('checkWorldBounds', true);
 
 
-    // Create player
+  //Map
+  //The image for the floor tiles (ex. 'floor')
+  //The image for the wall tiles (ex. 'wall')
+  //Minimum room size (in tiles) (ex.  2)
+  //Maximum room size (in tiles) (ex. 5)
+  //Maximum number of rooms possible (ex. 10)
+  //Player sprite so the map generator can place the player in a random place
+  this.map = new Map('floor','wall',2,5,10, player);
+  this.game.physics.game.world.setBounds(0,0,3000,3000);
 
-    //playergroup = game.add.group();
-    player = game.add.sprite(100, 100, 'player');
-    game.physics.arcade.enable(player);
-    player.body.collideWorldBounds = true;
-    player.anchor.set(0.5);
-    player.body.allowRotation = false;
 
-    // Create projectiles
-    projectiles = game.add.group();
-    projectiles.enableBody = true;
-    projectiles.physicsBodyType = Phaser.Physics.ARCADE;
-    // Create several projectiles for use
-    projectiles.createMultiple(50, 'projectile');
-    // Execute the reset function for each projectile that goes out of the world bounds
-    projectiles.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetProjectile);
-    projectiles.callAll('anchor.setTo','anchor', 0.5, 1.0);
-    projectiles.setAll('checkWorldBounds', true);
+  //Bring other sprites to top
+  game.world.bringToTop(player);
+  game.world.bringToTop(projectiles);
 
 
 }
 
 
-
 function update()
 {
-    // If the space bar is being pressed, generate a new dungeon
-    if ( space.isDown )
-    {
-        dungeonCreator.destroyMap();
-        dungeonCreator.createMap();
+  //Check collisions
+  game.physics.arcade.collide(player, this.map.walls);
+  game.physics.arcade.overlap(projectiles, this.map.walls, resetProjectile);
+
+
+  // Camera controls + camera follows the player
+  player.body.velocity.y = 0;
+  player.body.velocity.x = 0;
+
+  if(W_key.isDown) {
+    player.body.velocity.y -= 150;
+  }
+  else if(S_key.isDown) {
+    player.body.velocity.y += 150;
+  }
+  if(A_key.isDown) {
+    player.body.velocity.x -= 150;
+  }
+  else if(D_key.isDown) {
+    player.body.velocity.x += 150;
+  }
+  game.camera.follow(player);
+
+
+  // Player rotation + shooting
+  player.rotation = game.physics.arcade.angleToPointer(player);
+
+  if (game.input.activePointer.isDown) {
+    if (!mouseTouchDown) {
+      touchDown();
     }
-
-
-
-
-
-    // Camera controls + camera follows the player
-    player.body.velocity.y = 0;
-    player.body.velocity.x = 0;
-
-    if(W_key.isDown) {
-      player.body.velocity.y -= 150;
+  } else {
+    if (mouseTouchDown) {
+      touchUp();
     }
-    else if(S_key.isDown) {
-      player.body.velocity.y += 150;
-    }
-    if(A_key.isDown) {
-      player.body.velocity.x -= 150;
-    }
-    else if(D_key.isDown) {
-      player.body.velocity.x += 150;
-    }
-    game.camera.follow(player);
-
-
-    // Player rotation + shooting
-    player.rotation = game.physics.arcade.angleToPointer(player);
-
-    if (game.input.activePointer.isDown) {
-      if (!mouseTouchDown) {
-        touchDown();
-      }
-    } else {
-      if (mouseTouchDown) {
-        touchUp();
-      }
-    }
+  }
 
 }
 
@@ -126,11 +116,6 @@ function render()
 {
 	game.debug.text('FPS: ' + game.time.fps, 2, 14, "#00ff00");
 }
-
-
-
-
-
 
 
 function resetProjectile(projectile) {
