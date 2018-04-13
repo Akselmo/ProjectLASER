a5df3e432576f92b14fcadf7e3ba9e0b79fe476e
Aleksi Alajoki <fensnake@yahoo.com>
2018-04-13 20:05:13 +0300

    Enemy aiming + automatic shooting

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var game = new Phaser.Game(1280, 768, Phaser.AUTO, 'gameDiv', { preload: preload, create: create, update: update, render: render });
-var mouseTouchDown = false;
+game = new Phaser.Game(1280, 768, Phaser.AUTO, 'gameDiv', { preload: preload, create: create, update: update, render: render });
+mouseTouchDown = false;
+enemyID = 0;
 
 function preload()
 {
@@ -11,6 +12,7 @@ function preload()
   game.load.image('player', 'sprites/player.png');
   game.load.image('projectile', 'sprites/projectile.png');
   game.load.image('enemy', 'sprites/enemytest.png');
+  game.load.image('spawnPoint', 'sprites/spawnPoint.png');
 }
 
 
@@ -22,6 +24,11 @@ function create()
   A_key = game.input.keyboard.addKey(Phaser.Keyboard.A)
   S_key = game.input.keyboard.addKey(Phaser.Keyboard.S)
   D_key = game.input.keyboard.addKey(Phaser.Keyboard.D)
+
+
+  One = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+  Two = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
+  Three = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
 
   // Physics
   game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -58,6 +65,10 @@ function create()
   projectiles.setAll('checkWorldBounds', true);
 
 
+
+  enemyProjectiles = [];
+
+
   //Map
   //The image for the floor tiles (ex. 'floor')
   //The image for the wall tiles (ex. 'wall')
@@ -65,26 +76,53 @@ function create()
   //Maximum room size (in tiles) (ex. 5)
   //Maximum number of rooms possible (ex. 10)
   //Player sprite so the map generator can place the player in a random place
-  this.map = new Map('floor', 'wall', 2, 5, 10, player);
+
+
+  spawnPointsAmount = 3;
+  spawnPoints = game.add.group();
+
+  for (i = 0; i < spawnPointsAmount; i++) {
+    spawnPoint = spawnPoints.create(0, 0, 'spawnPoint');
+  }
+  //spawnPoint1 = spawnPoints.create(0, 0, 'spawnPoint');
+  //spawnPoint2 = spawnPoints.create(0, 0, 'spawnPoint');
+  //spawnPoint3 = spawnPoints.create(0, 0, 'spawnPoint');
+
+
+  this.map = new Map('floor', 'wall', 2, 5, 10, player, spawnPoints, 3);
   this.game.physics.game.world.setBounds(0,0,3000,3000);
 
   //Bring other sprites to top
+  game.world.bringToTop(spawnPoints);
   game.world.bringToTop(player);
   game.world.bringToTop(enemies);
   game.world.bringToTop(projectiles);
+  game.world.bringToTop(enemyProjectiles);
+
 
 
   mapwalls = this.map.walls;
-  // Spawn a single test enemy
-  spawnEnemy(500,500);
+
+
+  // Spawn testing
+  spawnEnemy(spawnPoints.getAt(0).x, spawnPoints.getAt(0).y);
+  spawnEnemy(spawnPoints.getAt(1).x, spawnPoints.getAt(1).y);
+  spawnEnemy(spawnPoints.getAt(2).x, spawnPoints.getAt(2).y);
 }
 
 function update()
 {
   //Check collisions
   game.physics.arcade.collide(player, this.map.walls);
+
   game.physics.arcade.overlap(enemies, projectiles, hitEnemy, null, this);
+
+  game.physics.arcade.overlap(player, enemyProjectiles, hitPlayer, null, this);
+
+
+  game.physics.arcade.overlap(player, enemies, contactDamage, null, this);
   game.physics.arcade.overlap(projectiles, this.map.walls, resetProjectile);
+  game.physics.arcade.overlap(enemyProjectiles, this.map.walls, resetProjectile);
 
 
   // Player controls + camera follows the player
@@ -106,6 +144,14 @@ function update()
   game.camera.follow(player);
 
 
+  // Testing enemy fire
+  if (One.isDown) {
+    fireEnemyProjectile(enemies.getAt(0).x, enemies.getAt(0).y, 0);
+  }
+  if (Two.isDown) {
+    fireEnemyProjectile(enemies.getAt(1).x, enemies.getAt(1).y, 1);
+  }
+
   // Player rotation + shooting
   player.rotation = game.physics.arcade.angleToPointer(player);
 
@@ -118,6 +164,18 @@ function update()
       touchUp();
     }
   }
+
+
+
+
+  // Enemies aim and shoot at the player
+  enemies.forEachExists(function(enemy) {
+      enemy.rotation = game.physics.arcade.angleToXY(enemy, player.x, player.y);
+
+        fireEnemyProjectile(enemy.x, enemy.y, enemy.id);
+      }
+    });
+
 
 }
 
@@ -133,30 +191,47 @@ function resetProjectile(projectile) {
 }
 
 function fireProjectile() {
-  var projectile = projectiles.getFirstExists(false);
+  projectile = projectiles.getFirstExists(false);
   if (projectile) {
     projectile.reset(player.x, player.y); // Projectile origin point
     game.physics.arcade.moveToPointer(projectile, 600); // Projectile speed
   }
 }
 
+
+
+
+
+
+function fireEnemyProjectile(x, y, enemyID) {
+
+  //projectile = enemyProjectiles.getAt(enemyID).getFirstExists(false);
+  projectile = enemyProjectiles[enemyID].getFirstExists(false);
+  if (projectile) {
+    projectile.reset(x, y); // Projectile origin point
+    game.physics.arcade.moveToObject(projectile, player, 600);
+  }
+}
+
+
+
+
+
 function hitEnemy(enemy, projectile) {
   enemy.damage(1);
   projectile.kill();
 }
 
-
-// WIP: make X and Y random, max values defined by map size
-function spawnEnemy(x, y) {
-  enemy = enemies.create(x, y, 'enemy');
-  enemies.set(enemy, 'health', 5);
-  enemies.set(enemy, 'checkWorldBounds', true);
-  if (game.physics.arcade.overlap(enemy, mapwalls)) {
-    enemy.kill();
-    // WIP: make the modifiers random / semi-random
-    spawnEnemy(x + 100, y + 100);
-  }
+function hitPlayer(player, projectile) {
+  player.damage(1);
+  projectile.kill();
 }
+function contactDamage() {
+  player.damage(1);
+}
+
+
+
 
 function touchDown() {
 	// Set touchDown to true, so we only trigger this once
@@ -167,4 +242,51 @@ function touchDown() {
 function touchUp() {
 	// Set touchDown to false, so we can trigger touchDown on the next click
 	mouseTouchDown = false;
+}
+
+
+
+
+
+
+function spawnEnemy(x, y) {
+   enemy = enemies.create(x, y, 'enemy');
+   enemy.anchor.set(0.5);
+   enemy.id = enemyID;
+   enemyID++;
+  enemies.set(enemy, 'health', 5);
+  enemies.set(enemy, 'checkWorldBounds', true);
+
+
+
+  // Create projectile array for the enemy
+  enemyProj = game.add.group();
+  enemyProj.enableBody = true;
+  enemyProj.physicsBodyType = Phaser.Physics.ARCADE;
+  // Create several projectiles for use
+  enemyProj.createMultiple(2, 'projectile');
+  // Execute the reset function for each projectile that goes out of the world bounds
+  enemyProj.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetProjectile);
+  enemyProj.callAll('anchor.setTo','anchor', 0.5, 1.0);
+  enemyProj.setAll('checkWorldBounds', true);
+  //enemyProjectiles.add(enemyProj);
+  enemyProjectiles.push(enemyProj);
+}
+
+
+
+
+
+
+
+// WIP: make X and Y random, max values defined by map size
+function spawnEnemyPrototype(x, y) {
+  enemy = enemies.create(x, y, 'enemy');
+  enemies.set(enemy, 'health', 5);
+  enemies.set(enemy, 'checkWorldBounds', true);
+  if (game.physics.arcade.overlap(enemy, mapwalls)) {
+    enemy.kill();
+    // WIP: make the modifiers random / semi-random
+    spawnEnemy(x + 100, y + 100);
+  }
 }
